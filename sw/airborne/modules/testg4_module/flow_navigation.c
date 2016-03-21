0a22eec71e5f411f39293583e2c2ecbdf0ed1e48
StevenH2812 <stevenhelm@live.nl>
2016-03-22 00:47:50 +0100

    Working automatic flight and wall avoidance in paparazzi simulation

--- sw/airborne/modules/testg4_module/flow_navigation.c
+++ sw/airborne/modules/testg4_module/flow_navigation.c
@@ -7,11 +7,19 @@
 
 int32_t incrementForAvoidance;
 uint8_t safeToGoForwards;
+float incrx = 0;
+float incry = 0.3;
 struct Line{
 	float a;
 	float b;
 	float c;
 };
+//struct Line line1;
+//struct Line line2;
+//struct Line line3;
+//struct Line line4;
+
+struct Line lines[4];
 
 
 struct Line constructLine(uint8_t wp_1,uint8_t wp_2){
@@ -20,11 +28,11 @@ struct Line constructLine(uint8_t wp_1,uint8_t wp_2){
 	float p1y = waypoint_get_y(wp_1);
 	float p2x = waypoint_get_x(wp_2);
 	float p2y = waypoint_get_y(wp_2);
-	printf("p1x,p1y,p2x,p2y are %f,%f,%f,%f\n",p1x,p1y,p2x,p2y);
+	//printf("p1x,p1y,p2x,p2y are %f,%f,%f,%f\n",p1x,p1y,p2x,p2y);
 	line.a = p1y-p2y;
 	line.b = p2x-p1x;
 	line.c = p1x*p2y - p2x*p1y;
-	printf("a,b,c are %f,%f,%f\n",line.a,line.b,line.c);
+	//printf("a,b,c are %f,%f,%f\n",line.a,line.b,line.c);
 	return line;
 }
 
@@ -41,6 +49,19 @@ float absol(float in){
 		return in*-1;
 	}
 }
+
+float capFun(float in, float upBound, float lowBound){
+	if(in>upBound){
+		return upBound;
+	}
+	else if(in<lowBound){
+		return lowBound;
+	}
+	else{
+		return in;
+	}
+}
+
 
 /**
  * Increases the NAV heading. Assumes heading is an INT32_ANGLE. It is bound in this function.
@@ -98,17 +119,52 @@ uint8_t chooseRandomIncrementAvoidance(){
 /**
  * Compute the distance between yourself (coordinates xself,yself) and a line given by y=ax+b
  */
-uint8_t distToLine(uint8_t wp_1, uint8_t wp_2){
-	struct Line line = constructLine(wp_1,wp_2);
-	float a = line.a;
-	float b = line.b;
-	float c = line.c;
-	struct EnuCoor_i *pos = stateGetPositionEnu_i(); // Get your current position
+uint8_t distToLine(uint8_t wp_target){
+	struct EnuCoor_f *pos = stateGetPositionEnu_f(); // Get your current position
 	float xself = (float)pos->x;
 	float yself = (float)pos->y;
-	float distance = absol(a*xself+b*yself+c)/sqrt(a*a+b*b);
-	printf("distance is %f\n",distance);
+	for(int i=0;i<4;i = i+1){
+		float a = lines[i].a;
+		float b = lines[i].b;
+		float c = lines[i].c;
+		float distance = absol(a*xself+b*yself+c)/sqrt(a*a+b*b);
+		printf("i is: %i distance is: %f\n",i,distance);
+		float closestx = (b*(b*xself-a*yself)-a*c)/(a*a+b*b);
+		float closesty = (a*(-b*xself+a*yself)-b*c)/(a*a+b*b);
+		float xinc = xself-closestx;
+		float yinc = yself-closesty;
+		float norm = sqrt((xinc*xinc+yinc*yinc));
+		xinc = xinc/norm;
+		yinc = yinc/norm;
+		printf("i is: %i, xinc is: %f, yinc is: %f\n",i,xinc,yinc);
+		float slow=0.1;
+		//float testx = incrx + slow*xinc*(1/(distance));
+		//float testy = incry + slow*yinc*(1/(distance));
+		//float mag = sqrt(testx*testx+testy*testy);
+		if(distance<1.8 ){
+			incrx = incrx + slow*xinc*(1/(distance));
+			incry = incry + slow*yinc*(1/(distance));
+		}
+	}
+	float wayx = waypoint_get_x(wp_target);
+	float wayy = waypoint_get_y(wp_target);
+	incrx = capFun(incrx,0.35,-0.35);
+	incry = capFun(incry,0.35,-0.35);
+	wayx = wayx+incrx;
+	wayy = wayy+incry;
+	waypoint_set_xy_i(wp_target,POS_BFP_OF_REAL(wayx),POS_BFP_OF_REAL(wayy));
+	bool_t temp = nav_set_heading_towards_waypoint(wp_target);
+	printf("incrx2 is: %f, incry2 is: %f\n",incrx,incry);
 	return FALSE;
 }
+
+uint8_t initialiseLines(uint8_t wp_1, uint8_t wp_2,uint8_t wp_3, uint8_t wp_4){
+	lines[0]=constructLine(wp_1,wp_2);
+	lines[1] = constructLine(wp_2,wp_3);
+	lines[2] = constructLine(wp_3,wp_4);
+	lines[3] = constructLine(wp_4,wp_1);
+	return FALSE;
+}
+
 
 
