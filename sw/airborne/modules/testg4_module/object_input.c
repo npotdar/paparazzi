82b825061b8ef93f677aa1e8eeff6fb1523feae2
StevenH2812 <stevenhelm@live.nl>
2016-03-23 15:13:35 +0100

    improvements to flying in the arena

---
+++ sw/airborne/modules/testg4_module/object_input.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include "object_input.h"
+
+
+#define n 10
+int fov = 3;
+float threshold = 10.0;
+float TURN = 0;
+//int TURN_R = 0;
+//int TURN_B = 0;
+int update = 1;
+
+double depth[n] =  {6.2, 42.5, 33.4, 52.2, 55.0, 5.2, 56.2, 8.5, 55.3, 2.1};
+
+float objTurnCommand(void)
+{
+	if (update==1){
+		int i;
+		for (i = n/2 -1 - fov ; i < n/2 + fov; i++){
+			if (depth[i] < threshold && TURN == 0){
+				if (i < n/2) {
+				TURN = 3;  //+1 turn right
+				}
+				else if (i >= n/2){
+				TURN = -3; //-1 turn left
+				}
+			}
+		}
+		update = 0;
+	}
+	else{
+		TURN = 0;
+	}
+	return TURN;
+}
---
+++ sw/airborne/modules/testg4_module/object_input.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: Mar 23, 2016
+ *      Author: mavlabcourse
+ */
+
+#ifndef OBJECT_INPUT_H
+#define OBJECT_INPUT_H
+
+extern float objTurnCommand(void);
+
+
+
+
+
+#endif /* TEST_H_ */
